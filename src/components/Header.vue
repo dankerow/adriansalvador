ca8b2a2e023c693a3347b706d93506f2859eed22
dankerow <78473524+dankerow@users.noreply.github.com>
2022-08-18 09:25:31 -0400

    ✨ (components): Added a "Header" component

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+	<header>
+		<nav :class="`navbar ${$colorMode.value === 'dark' ? 'navbar-dark' : '' } navbar-expand`">
+			<div class="container-fluid">
+				<div id="navbarCollapse" class="collapse navbar-collapse text-center">
+					<NuxtLink
+						class="navbar-brand mx-auto"
+						to="/"
+						aria-label="Navbar Logo"
+						no-prefetch
+					>
+						<span class="text text-uppercase">[Adrian Salvador]</span>
+						<span class="capton text-uppercase">{{ $t('metadata.description') }}</span>
+					</NuxtLink>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+header {
+	left: 0;
+	position: sticky;
+	right: 0;
+	top: 0;
+	width: 100%;
+	z-index: 9999;
+
+	.navbar {
+		background-color: rgb(245, 245, 245);
+
+		.navbar-brand {
+			user-select: none;
+
+			.text {
+				display: block;
+				font-weight: bold;
+				letter-spacing: 1em;
+				font-size: calc(10px + (16 - 10) * ((100vw - 300px) / (1600 - 300)));
+			}
+
+			.caption {
+				display: block;
+				font-size: 10px;
+				letter-spacing: 0.65em;
+				white-space: break-spaces;
+			}
+		}
+	}
+}
+
+.dark-mode {
+	header {
+		.navbar {
+			background-color: rgb(20, 20, 20);
+		}
+	}
+}
+</style>
