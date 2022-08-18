7f89887e74e58443a198b4128251c70eada81960
dankerow <78473524+dankerow@users.noreply.github.com>
2022-08-18 09:16:11 -0400

    ✨ (components): Added one "List" component to replace several components

---
+++ src/components/List.vue
@@ -0,0 +1,101 @@
+<template>
+	<div class="list">
+		<h3 class="title">
+			{{ title }}
+		</h3>
+
+		<ul class="list-unstyled">
+			<template v-if="!items.length">
+				<slot name="items" />
+			</template>
+			<template v-else>
+				<template v-for="item in items">
+					<li v-if="!item.hidden" :key="item.id">
+						<NuxtLink class="text" :to="item.url" no-prefetch>
+							{{ item.name }}
+						</NuxtLink>
+					</li>
+				</template>
+			</template>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true
+		},
+		items: {
+			type: Array,
+			default: () => []
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+	font-size: 12.5px;
+	font-weight: bold;
+	text-transform: uppercase;
+}
+
+ul {
+	width: auto;
+
+	li {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+
+		.text {
+			font-family: "Favorit SSENSE Inter", "Helvetica Neue", Helvetica, Arial, sans-serif;
+			font-size: 12px;
+			font-stretch: normal;
+			font-style: normal;
+			font-weight: normal;
+			letter-spacing: .09px;
+			line-height: 15px;
+			position: relative;
+			transition: color 300ms ease-in-out;
+
+			&:before {
+				bottom: 0;
+				background-color: currentColor;
+				content: "";
+				height: 0.5px;
+				left: auto;
+				position: absolute;
+				right: 0;
+				transition: width 0.2s linear;
+				width: 0;
+			}
+
+			&:hover {
+				&:before {
+					left: 0;
+					right: auto;
+					width: 100%;
+				}
+			}
+		}
+	}
+}
+
+.dark-mode {
+	ul {
+		li {
+			.text {
+				color: #9f9f9f;
+
+				&:hover {
+					color: #f5f5f5;
+				}
+			}
+		}
+	}
+}
+</style>
