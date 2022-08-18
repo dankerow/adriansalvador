782200026a86e60b7d329c94657674877ea0a685
dankerow <78473524+dankerow@users.noreply.github.com>
2022-08-18 09:24:41 -0400

    ✨ (locales): Added French translations

---
+++ src/locales/fr.js
@@ -0,0 +1,16 @@
+export default {
+	albums: {
+		buttons: {
+			share: {
+				metadata: {
+					title: 'Partager'
+				}
+			},
+			download: {
+				metadata: {
+					title: 'Télécharger'
+				}
+			}
+		}
+	}
+}
