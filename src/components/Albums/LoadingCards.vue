6c1dd1745a5f5384449699b76ad7048a1e19ec23
dankerow <78473524+dankerow@users.noreply.github.com>
2022-08-18 09:17:09 -0400

    ✨ (components/albums): Added loading cards placeholder

---
+++ src/components/Albums/LoadingCards.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="row justify-content-center g-4 mb-6">
+		<div v-for="(index) in 30" :key="index" class="col-auto">
+			<div class="image-container" />
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.image-container {
+	background: rgb(24, 24, 24);
+	height: 320px;
+	position: relative;
+	width: 225px;
+}
+</style>
