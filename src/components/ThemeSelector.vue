9969a02338208583fe29f0cfc82df0ff4ca3d84b
dankerow <78473524+dankerow@users.noreply.github.com>
2022-08-18 09:26:05 -0400

    ✨ (components): Added a "ThemeSelector" component

---
+++ src/components/ThemeSelector.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="btn-group">
+		<button class="btn btn-dark btn-sm" type="button" disabled>
+			Theme Preference:
+		</button>
+
+		<button class="btn btn-dark btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+			{{ $colorMode.preference }}
+		</button>
+
+		<ul :class="`dropdown-menu ${$colorMode.value === 'dark' ? 'dropdown-menu-dark' : '' }`">
+			<li
+				v-for="color of availableColors"
+				:key="color"
+				@click="$colorMode.preference = color"
+			>
+				<span class="dropdown-item text-capitalize">
+					{{ color }}
+				</span>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			colors: ['system', 'light', 'dark']
+		}
+	},
+	computed: {
+		availableColors() {
+			return this.colors.filter(color => color !== this.$colorMode.preference)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.btn-sm {
+	--bs-btn-padding-y: 0;
+	--bs-btn-padding-x: 0.25rem;
+	--bs-btn-font-size: 0.75rem;
+}
+
+.dropdown-toggle {
+	&:after {
+		margin-left: 0.055em;
+		vertical-align: 0.055em;
+	}
+}
+
+.dropdown-menu {
+	transition: all 0.3s ease-in-out;
+
+	.dropdown-item {
+		border-radius: 0.15rem;
+		cursor: pointer;
+		display: inline-block;
+		transition: all 0.3s ease-in-out;
+
+		&:not(:last-child) {
+			margin-bottom: 0.25rem;
+		}
+
+	}
+}
+</style>
