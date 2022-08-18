ee069f211cca70d7c594c3d2bb40d24de4f19cb2
dankerow <78473524+dankerow@users.noreply.github.com>
2022-08-18 09:23:18 -0400

    ✨ (locales): Added english translations

---
+++ src/locales/en.js
@@ -0,0 +1,19 @@
+export default {
+	metadata: {
+		description: '"Capturing the essence of people through a lens."'
+	},
+	albums: {
+		buttons: {
+			share: {
+				metadata: {
+					title: 'Share'
+				}
+			},
+			download: {
+				metadata: {
+					title: 'Download'
+				}
+			}
+		}
+	}
+}
