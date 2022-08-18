18fd981fe4749ba1980445457534fbd505dce828
dankerow <78473524+dankerow@users.noreply.github.com>
2022-08-18 09:22:05 -0400

    ✨ (layouts): Added a default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<LazyHydrate when-visible>
+			<Header />
+		</LazyHydrate>
+
+		<Nuxt role="main" />
+
+		<LazyHydrate when-visible>
+			<Footer />
+		</LazyHydrate>
+	</div>
+</template>
+
+<script>
+import LazyHydrate from 'vue-lazy-hydration'
+
+export default {
+	components: {
+		LazyHydrate
+	}
+}
+</script>
