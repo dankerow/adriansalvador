4b2a5ba1c9d75a5a66e0867b48f42572f6b2fcea
dankerow <78473524+dankerow@users.noreply.github.com>
2022-09-20 18:51:57 -0700

    ✨ (locales) Added French translations

--- src/locales/fr.js
+++ src/locales/fr.js
@@ -1,4 +1,8 @@
 export default {
+	metadata: {
+		description: '"Capturant l\'essence des gens à travers un objectif."'
+	},
+	biography: 'Cinéaste en herbe, né et élevé en Arabie saoudite, mais d\'origine philippine, Adrian est un cinéaste, un photographe et un artiste en général, ouvert et explorant de multiples médias artistiques. Son immersion et son intégration dans différentes cultures lui ont permis d\'avoir une perspective plus large du monde et de sa beauté. Il cherche à explorer des sujets qui évoquent de profondes émotions d\'amour et de douleur dans son art.',
 	albums: {
 		buttons: {
 			share: {
@@ -12,5 +16,18 @@ export default {
 				}
 			}
 		}
+	},
+	musics: {
+		metadata: {
+			title: 'Musiques'
+		}
+	},
+	videos: {
+		metadata: {
+			title: 'Vidéos'
+		}
+	},
+	cards: {
+		featured: 'En vedette'
 	}
 }
