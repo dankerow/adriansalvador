7560f76ad040bcd75d621fc20ae901ec6a5ac752
Dan <78473524+dankerow@users.noreply.github.com>
2023-02-12 21:27:45 -0800

    ✨ (composables): Added useFutch (shortcut for pre-configured useFetch)

---
+++ src/composables/useFutch.ts
@@ -0,0 +1,13 @@
+import { UseFetchOptions } from '#app'
+
+export default (url: string, options: UseFetchOptions<object> = {}) => {
+	const config = useRuntimeConfig()
+
+	return useFetch(url, {
+		baseURL: options.baseURL ?? config.public.apiBaseURL,
+		headers: {
+			accept: 'application/vnd.adriansalvador.json'
+		},
+		...options
+	})
+}
