c1157d8c79888c3f88c26b833868f9025375460b
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-25 13:40:41 -0700

    fix: remove footer actions

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,5 @@
 <template>
 	<footer>
-		<div class="footer-actions py-3">
-			<div class="container">
-				<div class="row align-items-center justify-content-center">
-					<div class="col-auto">
-						<ThemeSelector />
-					</div>
-					<div class="col-auto">
-						<LanguageSelector />
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<div class="footer-bottom py-3">
 			<div class="container">
 				<p class="mb-0">
@@ -30,11 +17,7 @@
 footer {
 	background-color: rgb(245, 245, 245);
 	color: #989898;
-	font-size: 12px;
-
-	.footer-actions {
-		background-color: rgb(235, 235, 235);
-	}
+	font-size: 0.75em;
 
 	.footer-bottom {
 		font-size: 12px;
@@ -53,7 +36,7 @@ footer {
 				left: auto;
 				bottom: 0;
 				position: absolute;
-				transition: width 0.2s linear;
+				transition: width 0.3s linear;
 			}
 
 			&:hover {
@@ -70,10 +53,6 @@ footer {
 .dark-mode {
 	footer {
 		background-color: rgb(20, 20, 20);
-
-		.footer-actions {
-			background-color: rgb(15, 15, 15);
-		}
 	}
 }
 </style>
