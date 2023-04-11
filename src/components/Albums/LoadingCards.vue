d0e45057e2953c88d0c4b1997af28a917b53fb00
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-11 10:56:56 -0700

    feat: redesigned albums loading cards

--- src/components/Albums/LoadingCards.vue
+++ src/components/Albums/LoadingCards.vue
@@ -1,7 +1,11 @@
 <template>
-	<div class="row justify-content-center g-4 mb-6">
-		<div v-for="(index) in 30" :key="index" class="col-auto">
-			<div class="image-container" />
+	<div class="row row-cols-5 justify-content-center g-4 mb-6">
+		<div v-for="(index) in 35" :key="index" class="col-auto">
+			<div class="image-container">
+				<div class="placeholder-glow">
+					<span class="placeholder col-12 placeholder-lg" />
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
