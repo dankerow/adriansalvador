ab35242ef39a4f2c369a248ed85b7c01d63ba14c
Dan <danker.twist@gmail.com>
2023-04-28 09:41:59 -0700

    feat: improve SEO (+ New album cover feature)

--- src/pages/albums/index.vue
+++ src/pages/albums/index.vue
@@ -2,12 +2,14 @@
 import { ComputedRef, Ref } from 'vue'
 import { Album } from '@/types/albums'
 
+const { t } = useI18n()
+
 useHead({
   title: 'Albums',
   meta: [
     {
       name: 'description',
-      content: 'Discover my albums.'
+      content: () => t('albums.description')
     },
     {
       name: 'og:title',
@@ -17,7 +19,7 @@ useHead({
     {
       name: 'og:description',
       property: 'og:description',
-      content: 'Discover my albums.'
+      content: () => t('albums.description')
     }
   ]
 })
@@ -27,7 +29,7 @@ const currentPage: Ref<number> = ref(1)
 
 const cdnBaseURL = useRuntimeConfig().public.cdnBaseURL
 
-const { pending: pendingAlbums, data: albums, error: errorAlbums  } = await useLazyAsyncData(() =>
+const { pending, data: albums, error  } = await useLazyAsyncData(() =>
   useFaetch('/albums', {
     params: {
       page: currentPage.value
@@ -39,6 +41,10 @@ watch(albums, (newAlbums) => {
   albums.value = newAlbums
   pages.value = newAlbums.pages
 })
+
+const getCoverUrl = (album: Album) => {
+  return album.cover ? `${cdnBaseURL}/covers/${encodeURIComponent(album.cover.name)}` : album.coverFallback ? `${cdnBaseURL}/gallery/${encodeURIComponent(album.name)}/${encodeURIComponent(album.coverFallback.name)}` : ''
+}
 
 const changePage = (index: number) => {
   window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
@@ -61,8 +67,20 @@ const getAlbumsView: ComputedRef<Album[]> = computed(() => {
       <Breadcrumb :links="[{ name: 'Albums' }]" class="mb-6" />
 
       <div class="container">
-        <div v-if="pendingAlbums" class="row row-cols-5 justify-content-center g-4 mb-6" data-aos="fade-up">
-          <div v-for="(index) in 35" :key="index" class="col">
+        <h1 class="h2 mb-3">
+          Capturing Memories: My Photography Albums
+        </h1>
+
+        <p class="mb-4">
+          Browse my collection of captivating albums and experience the beauty of our world through my lens.
+          From breathtaking landscapes to intimate portraits, my albums tell a story that will leave you in awe.
+          Explore now and see the world from a new perspective
+        </p>
+
+        <hr>
+
+        <div v-if="pending" class="row row-cols-5 justify-content-center g-4 mb-6" data-aos="fade-up">
+          <div v-for="(index) in 25" :key="index" class="col">
             <div class="card shadow-sm">
               <div class="card-img-top image-container d-flex align-items-center justify-content-center">
                 <h2>#?</h2>
@@ -71,7 +89,7 @@ const getAlbumsView: ComputedRef<Album[]> = computed(() => {
               <div class="card-body">
                 <div class="hstack">
                   <div>
-                    <Icon name="ph:folder-duotone" class="icon-link me-2" />
+                    <Icon name="ph:folder-duotone" class="me-2" />
                   </div>
                   <div>
                     <h4 class="card-title text-truncate mb-0">
@@ -83,18 +101,19 @@ const getAlbumsView: ComputedRef<Album[]> = computed(() => {
             </div>
           </div>
         </div>
+
         <div v-else-if="albums?.data" class="row row-cols-5 justify-content-center g-4 mb-6">
           <div v-for="album in getAlbumsView" :key="album.id" class="col">
             <div class="card shadow-sm">
               <div class="card-img-top image-container">
-                <nuxt-img v-if="album.cover" :src="`${cdnBaseURL}/images/${album.cover.name}?width=350`" class="img-fluid" :alt="`${album.name}'s thumbnail`" />
+                <nuxt-img :src="getCoverUrl(album)" width="350" fit="cover" class="img-fluid" placeholder :alt="`${album.name}'s thumbnail`" loading="lazy" />
               </div>
 
               <div class="card-body">
                 <div class="hstack gap-2">
                   <div>
-                    <Icon v-if="album.favorite || album.featured" name="ph:folder-star-duotone" class="icon-link" />
-                    <Icon v-else name="ph:folder-duotone" class="icon-link" />
+                    <Icon v-if="album.favorite || album.featured" name="ph:folder-star-duotone" />
+                    <Icon v-else name="ph:folder-duotone" />
                   </div>
                   <div class="text-truncate">
                     <h4 class="card-title mb-0">
@@ -102,7 +121,7 @@ const getAlbumsView: ComputedRef<Album[]> = computed(() => {
                     </h4>
                   </div>
                   <div class="ms-auto">
-                    <Icon name="material-symbols:open-in-new" class="open-new icon-link" size="1em" />
+                    <Icon name="material-symbols:open-in-new" class="open-new" size="1em" />
                   </div>
                 </div>
               </div>
@@ -115,6 +134,17 @@ const getAlbumsView: ComputedRef<Album[]> = computed(() => {
             </div>
           </div>
         </div>
+
+        <template v-else-if="error">
+          <div class="alert alert-danger" role="alert">
+            <h2 class="alert-heading h6 fw-bolder">
+              Something went wrong
+            </h2>
+            <p class="mb-0">
+              We couldn't load the images for this album. Please try again later.
+            </p>
+          </div>
+        </template>
       </div>
     </section>
 
