1f3da871679f1802192966897a01a77d197b2749
Dan <danker.twist@gmail.com>
2023-05-06 21:59:06 -0700

    feat: rewrite composable based on Nuxt custom useFetch example

--- src/composables/useFaetch.ts
+++ src/composables/useFaetch.ts
@@ -1,10 +1,14 @@
 import type { NitroFetchOptions } from 'nitropack'
+import defu from 'defu'
 
 export default <T>(url: string, options: NitroFetchOptions<Request> = {}): Promise<T> => {
   const config = useRuntimeConfig()
 
-  return $fetch(url, {
-    baseURL: config.apiBaseUrl,
-    ...options
-  })
+  const defaults: NitroFetchOptions<Request> = {
+    baseURL: config.apiBaseUrl
+  }
+
+  const params = defu(defaults, options)
+
+  return $fetch(url, params)
 }
