f51a54090ef487602a98f1f5d07bf48f55c04d9b
Dan <danker.twist@gmail.com>
2023-05-08 21:23:37 -0700

    feat: animate navbar on enter

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,36 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  fixed: boolean
+}>(), {
+  fixed: false
+})
+
+const { $gsap: gsap } = useNuxtApp()
+
+onMounted(() => {
+  gsap
+    .timeline()
+    .fromTo(
+      'header',
+      { opacity: 0, y: -100 },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 2.6,
+        duration: 1,
+        ease: 'back.out(0.2)',
+      }
+    )
+})
+</script>
+
 <template>
-  <header>
+  <header class="end-0 start-0 top-0 w-100 z-3" :class="{ 'position-sticky': !fixed, 'position-fixed': fixed }">
     <nav class="navbar navbar-expand">
       <div class="container-fluid">
         <div id="navbarCollapse" class="collapse navbar-collapse text-center">
           <NuxtLink
-            class="navbar-brand mx-auto"
+            class="navbar-brand user-select-none mx-auto"
             to="/"
             aria-label="Navbar Logo"
           >
@@ -19,42 +45,33 @@
 
 <style lang="scss" scoped>
 header {
-	left: 0;
-	position: sticky;
-	right: 0;
-	top: 0;
-	width: 100%;
-	z-index: 9999;
+  .navbar  {
+    backdrop-filter: blur(12px);
+    background-color: rgba(255, 255, 255, 0.8);
 
-	.navbar {
-		background-color: rgba(245, 245, 245, 0.8);
-		backdrop-filter: blur(12px);
+    .navbar-brand {
+      .text {
+        display: block;
+        font-weight: bold;
+        letter-spacing: 1em;
+        font-size: calc(10px + (16 - 10) * ((100vw - 300px) / (1600 - 300)));
+      }
 
-		.navbar-brand {
-			user-select: none;
-
-			.text {
-				display: block;
-				font-weight: bold;
-				letter-spacing: 1em;
-				font-size: calc(10px + (16 - 10) * ((100vw - 300px) / (1600 - 300)));
-			}
-
-			.caption {
-				display: block;
-				font-size: 10px;
-				letter-spacing: 0.65em;
-				white-space: break-spaces;
-			}
-		}
-	}
+      .caption {
+      display: block;
+      font-size: 10px;
+      letter-spacing: 0.65em;
+      white-space: break-spaces;
+      }
+    }
+  }
 }
 
 .dark-mode {
-	header {
-		.navbar {
-			background-color: rgba(14, 14, 14, 0.8);
-		}
-	}
+  header {
+    .navbar {
+      background-color: rgba(14, 14, 14, 0.8);
+    }
+  }
 }
 </style>
