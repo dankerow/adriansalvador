02682dcfdb4ee547545056352e11d9acbdcc47b7
Dan <danker.twist@gmail.com>
2023-05-08 21:22:32 -0700

    feat: remove Draggable plugin from GSAP

--- src/plugins/gsap.ts
+++ src/plugins/gsap.ts
@@ -1,11 +1,11 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { Draggable } from 'gsap/Draggable'
 
 export default defineNuxtPlugin(() => {
   if (process.client)
-    gsap.registerPlugin(ScrollTrigger, Draggable)
+    gsap.registerPlugin(ScrollTrigger)
+
   return {
-    provide: { gsap, ScrollTrigger, Draggable },
+    provide: { gsap, ScrollTrigger}
   }
 })
