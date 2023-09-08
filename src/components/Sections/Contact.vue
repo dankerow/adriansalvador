2cf13d957877d9adf5519641e7fa1ff6cf87ab3d
Dan <danker.twist@gmail.com>
2023-09-08 11:59:20 -0400

    feat: enhance contact section with personal info and animations

--- src/components/Sections/Contact.vue
+++ src/components/Sections/Contact.vue
@@ -1,12 +1,38 @@
 <template>
-  <section class="contact pt-8 pb-4 pt-lg-10 pb-lg-8 text-center">
+  <section class="contact pt-8 pb-4 pt-lg-10 pb-lg-8">
     <div class="container">
-      <div class="col-lg-7 mx-auto">
+      <div
+        v-motion="{
+          initial: { opacity: 0, y: 100 },
+          visible: {
+            y: 0,
+            opacity: 1
+          },
+          transition: {
+            delay: 1500
+          }
+        }"
+        class="card mb-16"
+      >
+        <div class="card-body pb-4">
+          <p class="card-text">
+            <span class="first-letter">A</span>
+            n aspiring film maker who was born and raised in Saudi Arabia, but originally Filipino, Adrian is a a filmmaker, photographer and overall artist open and exploring multiple medias of art. Through his immersion and integration into to different cultures he has been able to have a wider perspective of the world and its beauty. Aiming to explore subjects that evoke deep emotions of love and pain in his art.
+          </p>
+        </div>
+
+        <div class="card-footer text-body-secondary">
+          <Icon name="ph:map-pin-duotone" class="me-2" />
+          <span class="text-decoration-underline">Brisbane, Australia</span>
+        </div>
+      </div>
+
+      <div class="col-lg-7 mx-auto text-center">
         <h1 class="display-6 fw-bold text-body-emphasis">
           Any inquiries or collaborations?
         </h1>
         <p class="fs-3 mb-4">
-          {{ $t('contact.description') }}
+          Let's get in touch! I'm always eager to chat with you about anything related to my work. Whether you want to ask me something, share your ideas, or hire me for a project, I'm all ears!
         </p>
 
         <div class="d-grid gap-4 d-sm-flex justify-content-sm-center">
@@ -22,14 +48,28 @@
   </section>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .contact {
 	background: radial-gradient(ellipse at center, white 0%, #fafafa 100%);
+}
+
+p {
+  .first-letter {
+    font-weight: bold;
+    font-size: 52px;
+    line-height: normal;
+  }
 }
 
 .dark-mode {
 	.contact {
 		background: radial-gradient(ellipse at center, #080808 0%, #101010 100%);
 	}
+
+  p {
+    .first-letter {
+      color: #f5f5f5;
+    }
+  }
 }
 </style>
