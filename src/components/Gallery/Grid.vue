babb9f0e416bb33883992f24fcacb8f273643502
Dan <danker.twist@gmail.com>
2023-09-12 09:11:20 -0400

    feat: update Gallery Grid component for better UX
    Adapt component to use @yeger/vue-masonry-wall latest version
    Use responsive image sizes

--- src/components/Gallery/Grid.vue
+++ src/components/Gallery/Grid.vue
@@ -5,28 +5,33 @@ import PhotoSwipeLightbox from 'photoswipe/lightbox'
 import 'photoswipe/photoswipe.css'
 
 interface Props {
-  id: string,
-  images: any[],
-  ssrColumns?: number,
-  columnWidth?: number,
-  gap?: number,
+  images: any[]
+  ssrColumns?: number
+  columnWidth?: number
+  minColumns?: number
+  maxColumns?: number
+  gap?: number
+  scrollContainer?: HTMLElement | null
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  ssrColumns: 4,
-  columnWidth: 195,
-  gap: 15
+  columnWidth: 250,
+  scrollContainer: null,
+  gap: 20
 })
 
 const imagesData: Ref<any[]> = ref(props.images)
-const lightbox: Ref<PhotoSwipeLightbox|undefined> = ref()
+const lightbox: Ref<PhotoSwipeLightbox|null> = ref(null)
 
-onMounted(() => {
+const rootElement = ref<HTMLElement>()
+
+useSafeOnMounted(rootElement, () => {
   if (!lightbox.value) {
     lightbox.value = new PhotoSwipeLightbox({
-      gallery: `#${props.id}`,
-      children: '.image-container',
-      pswpModule: () => import('photoswipe')
+      gallery: rootElement.value.$el,
+      children: 'a',
+      pswpModule: () => import('photoswipe'),
+      wheelToZoom: true
     })
 
     lightbox.value?.on('uiRegister', () => {
@@ -35,22 +40,16 @@ onMounted(() => {
         order: 9,
         isButton: false,
         appendTo: 'root',
-        html: 'Caption text',
         onInit: (el: HTMLElement) => {
           lightbox.value?.pswp.on('change', () => {
             const currSlideElement = lightbox.value?.pswp.currSlide.data.element
-            let captionHTML = ''
 
             if (currSlideElement) {
               const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content') as HTMLElement
               if (hiddenCaption) {
-                captionHTML = hiddenCaption.innerHTML
-              } else {
-                captionHTML = currSlideElement.querySelector('img').getAttribute('alt')
+                el.innerHTML = hiddenCaption.innerHTML
               }
             }
-
-            el.innerHTML = captionHTML || ''
           })
         }
       })
@@ -61,39 +60,47 @@ onMounted(() => {
 })
 
 onUnmounted(() => {
-  if (lightbox.value) {
+  if (lightbox.value !== null) {
     lightbox.value?.destroy()
-    lightbox.value = undefined
+    lightbox.value = null
   }
+})
+
+watch(() => props.images, (newImages) => {
+  imagesData.value = newImages
 })
 </script>
 
 <template>
   <masonry-wall
-    :id="id"
+    ref="rootElement"
     :items="imagesData"
-    :ssr-columns="ssrColumns"
     :column-width="columnWidth"
+    :min-columns="minColumns"
+    :max-columns="maxColumns"
     :gap="gap"
+    :ssr-columns="ssrColumns"
+    :scroll-container="scrollContainer"
   >
     <template #default="{ item }">
-      <div class="image-container">
+      <div class="image-container" :style="{ height: `${((columnWidth + gap) / (item.width / item.height)).toFixed(0)}px` }">
         <a
           :href="item.url"
-          :data-pswp-src="item.url"
-          :data-pswp-width="item.metadata.width"
-          :data-pswp-height="item.metadata.height"
+          :data-pswp-width="item.width"
+          :data-pswp-height="item.height"
+          rel="noreferrer"
           aria-label="View image"
-          type="button"
         >
           <nuxt-img
-            class="image-thumbnail"
-            :src="`${item.thumb.url}?width=325`"
-            :width="325"
+            class="img-fluid"
+            format="webp"
+            :src="item.url"
+            :width="columnWidth + gap"
+            :height="(((columnWidth * 1.5) + gap) / (item.width / item.height)).toFixed(0)"
+            :sizes="`sm:100vw md:50vw lg:${(columnWidth) + gap}px xl:${((columnWidth) + gap) + 200}px`"
             loading="lazy"
             draggable="false"
-            style="height: 100%; width: 100%;"
-            alt=""
+            :alt="item.name"
           />
         </a>
 
@@ -107,23 +114,21 @@ onUnmounted(() => {
   </masonry-wall>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .image-container {
-	border-radius: 0.10rem;
-	background-color: #141414;
-	height: 100%;
-	overflow: hidden;
-	position: relative;
-	user-select: none;
-	width: fit-content;
+  border-radius: 0.10rem;
+  background-color: #141414;
+  overflow: hidden;
+  position: relative;
+  user-select: none;
 
-	img {
-		image-rendering: crisp-edges;
-		transition: transform .3s ease-in-out;
+  img {
+    image-rendering: crisp-edges;
+    transition: transform .3s ease-in-out;
 
-		&:hover {
-			transform: scale(1.05);
-		}
+    &:hover {
+     transform: scale(1.05);
+    }
 	}
 }
 </style>
