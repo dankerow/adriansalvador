73b04a509cef757bf184b07b258f022b2d9a03e2
Dan <danker.twist@gmail.com>
2023-09-12 09:05:22 -0400

    feat: remove i18n, and remove unrelated content from section (+ clean code)

--- src/components/Sections/Showcase.vue
+++ src/components/Sections/Showcase.vue
@@ -9,78 +9,44 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-  <section class="showcase min-vh-100 py-10 py-lg-12">
-    <div class="container">
+  <section class="showcase min-vh-100 h-100 pt-4 pb-10 py-lg-12">
+    <div class="container-fluid mb-12">
       <div
         v-motion="{
-          initial: { opacity: 0, y: 100 },
-          visible: {
-            y: 0,
-            opacity: 1
+          initial: {
+            opacity: 0,
+            x: -100
           },
-          transition: {
-            delay: 1500
+          visibleOnce: {
+            x: 0,
+            opacity: 1
           }
         }"
-        class="card mb-16"
+        class="ms-4"
       >
-        <div class="card-body pb-4">
-          <p class="card-text">
-            <span class="first-letter">{{ $t('biography')[0] }}</span>
-            {{ $t('biography').substring(1) }}
-          </p>
-        </div>
-
-        <div class="card-footer text-body-secondary">
-          <Icon name="ph:map-pin-duotone" class="me-2" />
-          <span class="text-decoration-underline">Brisbane, Australia</span>
-        </div>
+        <h1 class="display-5 fw-bold lh-1">
+          Showcase
+        </h1>
+        <p class="fs-3 lh-1">
+          Here you can find some of my films.
+        </p>
       </div>
+    </div>
 
+    <div class="container">
       <AlbumsFavorites :albums="albums" />
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
-.container-fluid {
-  --bs-gutter-x: 1rem;
-  --bs-gutter-y: 0;
-}
-
 .showcase {
-  background: radial-gradient(circle at center, white 0%, #fafafa 100%);
-
-  p {
-    .first-letter {
-      font-weight: bold;
-      font-size: 52px;
-      line-height: normal;
-    }
-
-    &:before {
-      content: '"';
-      font-size: 1.375em;
-      font-weight: bold;
-    }
-
-    &:after {
-      content: '"';
-      font-size: 1.375em;
-      font-weight: bold;
-    }
-  }
+  background-image: radial-gradient(circle at center, white 0%, #fafafa 100%);
 }
 
 .dark-mode {
   .showcase {
-    background: radial-gradient(circle at center, black 0%, #101010 100%);
-
-    p {
-      .first-letter {
-        color: #f5f5f5;
-      }
-    }
+    background-image: radial-gradient(circle at center, black 0%, #101010 100%);
   }
 }
 </style>
