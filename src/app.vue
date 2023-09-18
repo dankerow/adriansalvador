3a73fe5617ba7200943d2098810ab658cded651f
Dan <danker.twist@gmail.com>
2023-09-18 15:12:38 -0400

    feat: use app.vue instead of layouts

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,74 @@
 <script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'en-US',
+    dir: 'ltr'
+  },
+  title: null,
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Adrian Salvador` : 'Adrian Salvador | Photographer'
+  },
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'description', content: 'Capturing the essence of people through a lens.' },
+    {
+      name: 'twitter:card',
+      content: 'summary'
+    },
+    {
+      name: 'twitter:site',
+      content: 'Adrian Salvador | Photographer'
+    },
+    {
+      name: 'twitter:creator',
+      content: '@dankerow'
+    },
+    {
+      name: 'twitter:title',
+      content: 'salvadoradrian.com'
+    },
+    {
+      name: 'twitter:description',
+      content: 'Capturing the essence of people through a lens.'
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://salvadoradrian.com/icon.png'
+    },
+    {
+      name: 'og:title',
+      property: 'og:title',
+      content: 'Adrian Salvador | Photographer'
+    },
+    {
+      name: 'og:description',
+      property: 'og:description',
+      content: 'Capturing the essence of people through a lens.'
+    },
+    {
+      name: 'og:image',
+      property: 'og:image',
+      content: 'https://salvadoradrian.com/icon.png'
+    }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'preconnect', href: 'https://cdn.salvadoradrian.com' },
+    { rel: 'dns-prefetch', href: 'https://cdnjs.cloudflare.com' }
+  ],
+  script: [
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/js/bootstrap.bundle.min.js',
+      type: 'text/javascript',
+      defer: true
+    }
+  ]
+})
+
 const { $gsap: gsap, $ScrollTrigger: ScrollTrigger } = useNuxtApp()
 const { page } = useCounter()
+const colorMode = useColorMode()
+const route = useRoute()
 
 const hidden = ref(true)
 
@@ -72,38 +140,55 @@ const onLeave = (el, done) => {
       }
     })
 }
+
+const isHome = computed(() => {
+  return route.path === '/'
+})
+
+onMounted(() => {
+  const setTheme = function(theme: string) {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+  }
+
+  setTheme(colorMode.value)
+})
 </script>
 
 <template>
   <div>
-    <NuxtLayout>
-      <svg
-        id="transition"
-        xmlns="http://www.w3.org/2000/svg"
-        class="position-fixed start-0 top-0 vh-100 vw-100 text-black z-3"
-        :class="{ 'd-none': hidden }"
-        fill="currentColor"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-      >
-        <path
-          id="quadbz"
-          stroke="transparent"
-          vector-effect="non-scaling-stroke"
-          d="M 0 100 V 100 Q 50 100 100 100 V 100 z"
-        />
-      </svg>
+    <Loader />
+    <LazyCursor />
 
-      <NuxtPage
-        :transition="{
-          mode: 'out-in',
-          css: false,
-          onEnter,
-          onAfterEnter,
-          onBeforeLeave,
-          onLeave
-        }"
+    <Header v-show="!isHome" />
+
+    <svg
+      id="transition"
+      xmlns="http://www.w3.org/2000/svg"
+      class="position-fixed start-0 top-0 vh-100 vw-100 text-black z-3"
+      :class="{ 'd-none': hidden }"
+      fill="currentColor"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+    >
+      <path
+        id="quadbz"
+        stroke="transparent"
+        vector-effect="non-scaling-stroke"
+        d="M 0 100 V 100 Q 50 100 100 100 V 100 z"
       />
-    </NuxtLayout>
+    </svg>
+
+    <NuxtPage
+      :transition="{
+        mode: 'out-in',
+        css: false,
+        onEnter,
+        onAfterEnter,
+        onBeforeLeave,
+        onLeave
+      }"
+    />
+
+    <Footer />
   </div>
 </template>
