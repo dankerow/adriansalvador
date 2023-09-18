e25ec84a450295aa1a4f67311bc2e9eb1a57e81b
Dan <danker.twist@gmail.com>
2023-09-18 15:06:24 -0400

    fear: removed previous on-scroll fixed position feature and clean code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,5 @@
-<script setup lang="ts">
-withDefaults(defineProps<{
-  fixed: boolean
-}>(), {
-  fixed: false
-})
-
-const { $gsap: gsap } = useNuxtApp()
-
-onMounted(() => {
-  gsap
-    .timeline()
-    .fromTo(
-      'header',
-      { opacity: 0, y: -100 },
-      {
-        opacity: 1,
-        y: 0,
-        delay: 2.6,
-        duration: 1,
-        ease: 'back.out(0.2)',
-      }
-    )
-})
-</script>
-
 <template>
-  <header class="end-0 start-0 top-0 w-100 z-3" :class="{ 'position-sticky': !fixed, 'position-fixed': fixed }">
+  <header class="position-fixed end-0 start-0 top-0 w-100 z-3">
     <nav class="navbar navbar-expand">
       <div class="container-fluid">
         <div id="navbarCollapse" class="collapse navbar-collapse text-center">
@@ -34,8 +8,9 @@ onMounted(() => {
             to="/"
             aria-label="Navbar Logo"
           >
-            <span class="text text-uppercase">[Adrian Salvador]</span>
-            <span class="caption text-uppercase">{{ $t('metadata.description') }}</span>
+            <span class="text d-block text-uppercase fw-bold">
+              Adrian Salvador
+            </span>
           </NuxtLink>
         </div>
       </div>
@@ -51,17 +26,14 @@ header {
 
     .navbar-brand {
       .text {
-        display: block;
-        font-weight: bold;
         letter-spacing: 1em;
         font-size: calc(10px + (16 - 10) * ((100vw - 300px) / (1600 - 300)));
       }
 
       .caption {
-      display: block;
-      font-size: 10px;
-      letter-spacing: 0.65em;
-      white-space: break-spaces;
+        font-size: 10px;
+        letter-spacing: 0.65em;
+        white-space: break-spaces;
       }
     }
   }
