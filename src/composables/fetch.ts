ef5d366c914d9998d91bfe1fda951838741f2fa3
Dan <danker.twist@gmail.com>
2023-10-02 20:01:45 -0400

    feat: refactor fetch functionalities into one file
    Files useFaetch.ts and useFutch.ts were deleted and their functionalities were moved into a new file called fetch.ts. This improves maintainability of the code by keeping all fetching related logic in one place. In addition, withLoadingState has been added to handle loading states during fetches.

---
+++ src/composables/fetch.ts
@@ -0,0 +1,28 @@
+import type { UseFetchOptions } from '#app'
+import type { NitroFetchOptions } from 'nitropack'
+import defu from 'defu'
+
+export const useFaetch = <T>(url: string, options: NitroFetchOptions<Request> = {}): Promise<T> => {
+  const config = useRuntimeConfig()
+
+  const defaults: NitroFetchOptions<Request> = {
+    baseURL: config.apiBaseUrl
+  }
+
+  const params = defu(options, defaults)
+
+  return $fetch(url, params)
+}
+
+export const useFutch = <T>(url: string, options: UseFetchOptions<T> = {}) => {
+  const config = useRuntimeConfig()
+
+  const defaults: UseFetchOptions<T> = {
+    baseURL: config.public.apiBaseUrl,
+    key: url
+  }
+
+  const params = defu(options, defaults)
+
+  return useFetch(url, params)
+}
