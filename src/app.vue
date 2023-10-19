7b85cc5f6353978e00eb5727d6f2c59b4c179617
Dan <danker.twist@gmail.com>
2023-10-19 17:30:04 -0400

    feat: update Bootstrap scripts to v5.3.2

--- src/app.vue
+++ src/app.vue
@@ -58,7 +58,7 @@ useHead({
   ],
   script: [
     {
-      src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/js/bootstrap.bundle.min.js',
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.bundle.min.js',
       type: 'text/javascript',
       defer: true
     }
