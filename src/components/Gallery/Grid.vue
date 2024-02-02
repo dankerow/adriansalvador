d2487086fd7d13d856715949d8a00dfceb96f1d2
Dan <danker.twist@gmail.com>
2024-02-02 13:32:30 -0500

    feat: calculate image dimensions based on the available space in the masonry layout

--- src/components/Gallery/Grid.vue
+++ src/components/Gallery/Grid.vue
@@ -22,6 +22,26 @@ const imagesData = computed<any[]>(() => props.images)
 const lightbox = ref<PhotoSwipeLightbox | null>(null)
 const masonry = ref<HTMLElement>()
 
+const getImageDimensions = (image: any) => {
+  const width = image.metadata.width
+  const height = image.metadata.height
+
+  const ratio = width / height
+
+  const columnWidth = props.columnWidth
+  const gap = props.gap
+
+  const columns = Math.floor((masonry.value?.$el.clientWidth || 0) / (columnWidth + gap))
+
+  const imageWidth = Math.min((masonry.value?.$el.clientWidth || 0) / columns, columnWidth)
+  const imageHeight = imageWidth / ratio
+
+  return {
+    width: imageWidth,
+    height: imageHeight
+  }
+}
+
 onMounted(() => {
   if (!lightbox.value && masonry.value) {
     lightbox.value = new PhotoSwipeLightbox({
@@ -32,7 +52,7 @@ onMounted(() => {
     })
 
     lightbox.value?.on('uiRegister', () => {
-      lightbox.value?.pswp.ui.registerElement({
+      lightbox.value?.pswp?.ui?.registerElement({
         name: 'custom-caption',
         order: 9,
         isButton: false,
@@ -76,30 +96,29 @@ onUnmounted(() => {
     :scroll-container="scrollContainer"
   >
     <template #default="{ item }">
-      <a
-        :href="item.url"
-        :data-pswp-width="item.metadata.width"
-        :data-pswp-height="item.metadata.height"
-        rel="noreferrer"
-        aria-label="View image"
-      >
-        <nuxt-img
-          class="w-100"
-          format="webp"
-          :src="item.url"
-          :width="columnWidth + 50 + gap"
-          :height="((columnWidth + 50 + gap) / (item.metadata.width / item.metadata.height)).toFixed(0)"
-          :sizes="`sm:50vw md:${(columnWidth) + gap}px lg:${(columnWidth) + gap + 100}px xl:${((columnWidth) + gap) + 200}px`"
-          loading="lazy"
-          draggable="false"
-          :alt="item.name"
-        />
-      </a>
+      <div class="img-container" :style="{ width: `${getImageDimensions(item).width}px`, height: `${getImageDimensions(item).height}px` }">
+        <a
+          :href="item.url"
+          :data-pswp-width="item.metadata.width"
+          :data-pswp-height="item.metadata.height"
+          rel="noreferrer"
+          aria-label="View image"
+        >
+          <nuxt-img
+            format="webp"
+            :src="item.url"
+            :sizes="`sm:50vw md:${getImageDimensions(item).width}px lg:${(getImageDimensions(item).width) + 100}px xl:${(getImageDimensions(item).width) + 200}px`"
+            loading="lazy"
+            draggable="false"
+            :alt="item.name"
+          />
+        </a>
 
-      <div v-if="item.album" class="hidden-caption-content">
-        <NuxtLink :to="`/albums/${item.album.id}`">
-          {{ item.album.name }}
-        </NuxtLink>
+        <div v-if="item.album" class="hidden-caption-content">
+          <NuxtLink :to="`/albums/${item.album.id}`">
+            {{ item.album.name }}
+          </NuxtLink>
+        </div>
       </div>
     </template>
   </masonry-wall>
@@ -118,8 +137,8 @@ onUnmounted(() => {
     transition: transform .3s ease-in-out;
 
     &:hover {
-     transform: scale(1.05);
+      transform: scale(1.05);
     }
-	}
+  }
 }
 </style>
