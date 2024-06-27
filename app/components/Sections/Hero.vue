02e44673e7d5d4cedb677fb350cc465b816f2ce5
Dan <danker.twist@gmail.com>
2024-06-26 20:52:35 -0700

    feat: redesign hero section

--- app/components/Sections/Hero.vue
+++ app/components/Sections/Hero.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 const colorMode = useColorMode()
 
+const navLinks = [
+  { title: 'home', to: '/' },
+  { title: 'my work', to: '/work' },
+  { title: 'about', to: '/about' }
+]
+
 onMounted(() => {
   const gradient = new Gradient()
 
@@ -13,49 +19,84 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="hero min-vh-100 h-100 position-relative">
-    <canvas id="gradient-canvas-hero" data-transition-in />
+  <section class="hero min-vh-100 p-4">
+    <div class="cover position-relative rounded-4">
+      <canvas id="gradient-canvas-hero" data-transition-in />
 
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon" />
-        </button>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid px-3">
+          <NuxtLink
+              class="navbar-brand user-select-none mx-auto"
+              to="/"
+              aria-label="Navbar Logo"
+          >
+            <span class="text-uppercase">
+              Adrian Salvador
+            </span>
+          </NuxtLink>
 
-        <div id="navbarSupportedContent" class="collapse navbar-collapse">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item me-3">
-              <NuxtLink class="nav-link" to="/albums">
-                Albums
-              </NuxtLink>
-            </li>
+          <button
+              class="navbar-toggler border-0"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarContent"
+              aria-controls="navbarContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+          >
+            <Icon
+                name="solar:hamburger-menu-line-duotone"
+                size="1.8em"
+            />
+          </button>
 
-            <li class="nav-item me-3">
-              <a class="nav-link" href="https://www.instagram.com/adriansalvadore/" rel="noreferrer" target="_blank" aria-label="My Instagram Profile">
-                <Icon name="ph:instagram-logo-duotone" size="1.5em" />
-              </a>
-            </li>
+          <div id="navbarContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav nav-underline text-uppercase ms-auto mb-2 mb-lg-0">
+              <li
+                  v-for="link in navLinks"
+                  :key="link.to"
+                  class="nav-item"
+              >
+                <NuxtLink
+                    :to="link.to"
+                    class="nav-link"
+                    :class="{ active: $route.path === link.to }"
+                >
+                  {{ link.title }}
+                </NuxtLink>
+              </li>
+            </ul>
 
-            <li class="nav-item me-3">
-              <a class="nav-link" href="https://vimeo.com/user181229489" rel="noreferrer" target="_blank" aria-label="My Vimeo Profile">
-                <Icon name="ph:video-duotone" size="1.5em" />
-              </a>
-            </li>
-          </ul>
+            <div class="vr my-auto mx-3" />
+
+            <ul class="navbar-nav list-unstyled">
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.instagram.com/adriansalvadore/" rel="noreferrer" target="_blank" aria-label="My Instagram Profile">
+                  <Icon name="ph:instagram-logo-duotone" size="1.4em" />
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="https://vimeo.com/user181229489" rel="noreferrer" target="_blank" aria-label="My Vimeo Profile">
+                  <Icon name="ph:video-duotone" size="1.4em" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
 
-    <div class="container-fluid">
-      <div class="bottom position-absolute bottom-0 start-0 text-uppercase">
-        <h1 class="fw-bold lh-1 ms-4">
-          Adrian
-          Salvador
-        </h1>
+      <div class="container-fluid">
+        <div class="bottom position-absolute bottom-0 start-0 text-uppercase">
+          <h1 class="fw-bold lh-1 ms-4">
+            Adrian
+            Salvador
+          </h1>
 
-        <p class="lh-1 ms-4 mb-8">
-          "Capturing the essence of people through a lens."
-        </p>
+          <p class="lead text-body-secondary fst-italic ms-4 mb-6">
+            Capturing the essence of people through a lens.
+          </p>
+        </div>
       </div>
     </div>
   </section>
@@ -63,15 +104,6 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 .hero {
-  &::before {
-    background-size: 2rem 2rem;
-    content: '';
-    height: 100%;
-    position: absolute;
-    width: 100%;
-    z-index: 0;
-  }
-
   .bottom {
     h1 {
       font-size: calc(1rem + 6vw);
@@ -81,6 +113,11 @@ onMounted(() => {
       font-size: calc(1rem + 1vw);
     }
   }
+}
+
+.cover {
+  overflow: hidden;
+  height: 90vh;
 }
 
 #gradient-canvas-hero {
