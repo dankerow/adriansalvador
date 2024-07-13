6afaf36905b0c77a7ed00c1923448a87d095283f
Dan <danker.twist@gmail.com>
2024-07-13 15:57:37 -0700

    feat: adjust albums skeleton layout

--- app/components/Albums/LoadingCards.vue
+++ app/components/Albums/LoadingCards.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row row-cols-5 justify-content-center g-4 mb-6">
+  <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 justify-content-center g-4 mb-6">
     <div v-for="(index) in 25" :key="index" class="col">
       <div class="image-container">
         <div class="placeholder-glow">
